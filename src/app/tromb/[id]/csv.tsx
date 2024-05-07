616a624a8b9a56e1d31c83381712cb5d9da2c640
anthoeptm <anthoeptm@gmail.com>
2024-05-07 16:23:09 +0200

    wip: read csv

--- src/app/tromb/[id]/csv.tsx
+++ src/app/tromb/[id]/csv.tsx
@@ -1,7 +1,25 @@
 export const Csv = () => {
 
-    const onFileChanged : React.ChangeEventHandler<HTMLInputElement> = (e) => {
-        console.log(e)
+    const onFileChanged : React.ChangeEventHandler<HTMLInputElement> = async (e) => {
+        if (!e.target.files || !e.target.files[0]) return
+        let formData = new FormData();
+        const fr = new FileReader();
+
+        fr.onload = async () => {
+            const a = fr.result;
+            console.log(a);
+            
+            formData.append("file", a!.toString())
+            const res = await fetch("/api/uploadcsv", {
+                method: "post",
+                body: formData
+            });
+            res.json().then(r => {
+                console.log(r)
+            })
+        }
+
+        fr.readAsText(e.target.files[0])
     }
 
     return (
