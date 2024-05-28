a2be2ceb8b6867249daebc108a6fb57a1f113f3f
raphou <rey29raphael@gmail.com>
2024-05-28 16:27:29 +0200

    jgfb

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,11 +1,11 @@
 import lucia from "@/libs/lucia";
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-const GET = () => {
+const GET = (req: NextRequest) => {
     const sessionCookie = lucia.createBlankSessionCookie();
     cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-    return NextResponse.redirect(process.env.APP_URL!)
+    return NextResponse.redirect(req.url)
 }
 
 export { GET }
