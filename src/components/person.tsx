dfeb6b19c3f9f6b79353cb699d7e2e1a107c3da1
raphou <rey29raphael@gmail.com>
2024-05-28 15:54:28 +0200

    Letshooooooooo

--- src/components/person.tsx
+++ src/components/person.tsx
@@ -43,7 +43,7 @@ const PersonEl = (person: any) => {
                         </div>
                         <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                             <h3 className="text-base font-semibold leading-6">
-                                Suprimer l'utilisateur
+                                Suprimer l&apos;utilisateur
                             </h3>
                             <div className="mt-2">
                                 <p className="text-sm text-gray-500">
--- src/components/tromb.tsx
+++ src/components/tromb.tsx
@@ -105,7 +105,7 @@ const TrombEl = (tromb: any) => {
                         </div>
                         <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                             <h3 className="text-base font-semibold leading-6">
-                                Suprimer l'utilisateur
+                                Suprimer le trombinoscope
                             </h3>
                             <div className="mt-2">
                                 <p className="text-sm text-gray-500">
